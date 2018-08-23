0b92cded84d0daca410bcb6cd7060c669fe31fce
ChrisNate <seres210403@hotmail.com>
2018-08-23 17:11:44 +0200

    Personajes bloqueados

--- core/src/com/mygdx/game/Logica/GameProgress.java
+++ core/src/com/mygdx/game/Logica/GameProgress.java
@@ -2,6 +2,7 @@ package com.mygdx.game.Logica;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Preferences;
+import com.mygdx.game.Logica.Objetos.CharacterRecord;
 
 public class GameProgress {
 
@@ -21,6 +22,11 @@ public class GameProgress {
     private static final String SAVE_KEY_PLAYER_DAMAGE="playerdamage";
     private static final String SAVE_KEY_GOLD="currentgold";
     private static final String SAVE_KEY_CURRENT_CHARACTER = "currentcharacter";
+    private static final String SAVE_KEY_PLAYER_LEVEL="playerlevel";
+
+    public static final int CHARACTER_PRICE=1000;
+    public static int levels[]; // level of each character, 0= locked
+
 
     public static int getEnemyLives(){
 
@@ -36,11 +42,17 @@ public class GameProgress {
         prefs.putInteger(SAVE_KEY_PLAYER_DAMAGE, playerDamage);
         prefs.putInteger(SAVE_KEY_GOLD, currentGold);
         prefs.putInteger(SAVE_KEY_CURRENT_CHARACTER, currentCharacter);
+        for(int i=0; i<levels.length; i++ ){
+            prefs.putInteger(SAVE_KEY_PLAYER_LEVEL + i, levels[i] );
+        }
+
         prefs.flush();
 
     }
 
     public static void Load(){
+
+        levels=new int[CharacterRecord.CHARACTERS.length];
 
         Preferences prefs= Gdx.app.getPreferences(PROGRESS_SAVE_NAME);
         playerLives=prefs.getInteger(SAVE_KEY_LIVES, 3);
@@ -49,6 +61,11 @@ public class GameProgress {
         currentLevel=prefs.getInteger(SAVE_KEY_CURRENT_LEVEL, 0);
         currentCharacter=prefs.getInteger(SAVE_KEY_CURRENT_CHARACTER, 0);
         currentGold=prefs.getInteger(SAVE_KEY_GOLD, 0);
+        for(int i=0; i<CharacterRecord.CHARACTERS.length; i++ ){
+
+            levels[i]=prefs.getInteger(SAVE_KEY_PLAYER_LEVEL +i, i==0 ? 1:0);
+        }
+
     }
 
 
@@ -58,7 +75,10 @@ public class GameProgress {
         maxPlayerLive=3;
         currentLevel=0;
         playerDamage=1;
-        currentGold=0;
         currentCharacter=0;
+    }
+
+    public static int getNextUpgradeCost(int currentCharacter) {
+        return levels[currentCharacter]*2;
     }
 }
--- core/src/com/mygdx/game/Pantallas/CharacterSelectionScreen.java
+++ core/src/com/mygdx/game/Pantallas/CharacterSelectionScreen.java
@@ -33,27 +33,84 @@ public class CharacterSelectionScreen extends DefaultScreen {
         TextButton.TextButtonStyle buttonStyle=new TextButton.TextButtonStyle();
         buttonStyle.font= juego.res.gameFont;
         buttonStyle.font.getData().setScale(0.4f);
-
         buttonStyle.fontColor= Color.WHITE;
 
-        TextButton startButton=new TextButton("START", buttonStyle);
+        uiStage.clear();
 
-        startButton.setPosition((uiStage.getWidth()-startButton.getWidth())/2, uiStage.getHeight()/6);
-        startButton.addListener(new ClickListener()
-                                {
-                                    @Override
-                                    public void touchUp(InputEvent event, float x, float y, int pointer, int button ){
+        Label.LabelStyle labelStyle=new Label.LabelStyle(juego.res.gameFont, Color.WHITE);
+        labelStyle.font.getData().setScale(0.4f);
 
-                                        dispose();
-                                        juego.setScreen(new GameScreen(juego));
+        if(GameProgress.levels[GameProgress.currentCharacter]==0){ // char is locked
+
+            TextButton unlockButton=new TextButton("UNLOCK(1000 GOLD)", buttonStyle);
+            unlockButton.setPosition((uiStage.getWidth()-unlockButton.getWidth())/2, uiStage.getHeight()*5/6);
+            unlockButton.addListener(new ClickListener()
+                                    {
+                                        @Override
+                                        public void touchUp(InputEvent event, float x, float y, int pointer, int button ){
+
+                                            if(GameProgress.currentGold>=GameProgress.CHARACTER_PRICE){
+
+                                                GameProgress.currentGold-=GameProgress.CHARACTER_PRICE;
+                                                GameProgress.levels[GameProgress.currentCharacter]=1;
+                                                prepareUI();
+                                            }
+
+                                        }
+
                                     }
+            );
+            uiStage.addActor(unlockButton);
 
-                                }
-        );
-        uiStage.addActor(startButton);
+        }else{
+
+            TextButton upgradeButton=new TextButton("LvlUp(" + GameProgress.getNextUpgradeCost(GameProgress.currentCharacter)+")", buttonStyle);
+            upgradeButton.setPosition((uiStage.getWidth()-upgradeButton.getWidth())/2, uiStage.getHeight()/6);
+            upgradeButton.addListener(new ClickListener()
+                                      {
+                                          @Override
+                                          public void touchUp(InputEvent event, float x, float y, int pointer, int button ){
+
+                                              if(GameProgress.currentGold>=GameProgress.getNextUpgradeCost(GameProgress.currentCharacter)){
+
+                                                  GameProgress.currentGold-=GameProgress.getNextUpgradeCost(GameProgress.currentCharacter);
+                                                  GameProgress.levels[GameProgress.currentCharacter]+=1;
+                                                  prepareUI();
+                                              }
+
+                                          }
+
+                                      }
+            );
+            uiStage.addActor(upgradeButton);
+
+            TextButton startButton=new TextButton("START", buttonStyle);
+
+            startButton.setPosition((uiStage.getWidth()-startButton.getWidth())/2, uiStage.getHeight()*5/6);
+            startButton.addListener(new ClickListener()
+                                    {
+                                        @Override
+                                        public void touchUp(InputEvent event, float x, float y, int pointer, int button ){
+
+                                            dispose();
+                                            juego.setScreen(new GameScreen(juego));
+                                        }
+
+                                    }
+            );
+            uiStage.addActor(startButton);
+
+        }
+
+
         Image heroSprite= new Image(juego.res.playerSprites.get(CharacterRecord.CHARACTERS[GameProgress.currentCharacter].name));
         heroSprite.setPosition((uiStage.getWidth()-heroSprite.getWidth())/2, (uiStage.getHeight()-heroSprite.getHeight())/2 );
         uiStage.addActor(heroSprite);
+
+        int lvl=GameProgress.levels[GameProgress.currentCharacter];
+        Label statusText=new Label(lvl>0 ? "LVL: " + lvl : "LOCKED", labelStyle);
+        statusText.setPosition(heroSprite.getX()+(heroSprite.getWidth()-statusText.getWidth())/2, heroSprite.getY()-statusText.getHeight()-5 );
+        uiStage.addActor(statusText);
 
         TextButton nextButton=new TextButton(">>>", buttonStyle);
         nextButton.setPosition(uiStage.getWidth()*5/6 - nextButton.getWidth()/2, uiStage.getHeight()/2);
@@ -64,7 +121,6 @@ public class CharacterSelectionScreen extends DefaultScreen {
 
                                         GameProgress.currentCharacter++;
                                         if(GameProgress.currentCharacter==CharacterRecord.CHARACTERS.length)GameProgress.currentCharacter=0;
-                                        uiStage.clear();
                                         prepareUI();
                                     }
 
@@ -82,7 +138,6 @@ public class CharacterSelectionScreen extends DefaultScreen {
 
                                        GameProgress.currentCharacter--;
                                        if(GameProgress.currentCharacter<0)GameProgress.currentCharacter=CharacterRecord.CHARACTERS.length-1;
-                                       uiStage.clear();
                                        prepareUI();
                                    }
 
@@ -94,8 +149,7 @@ public class CharacterSelectionScreen extends DefaultScreen {
         coinImage.setPosition(1,1);
         uiStage.addActor(coinImage);
 
-        Label.LabelStyle labelStyle=new Label.LabelStyle(juego.res.gameFont, Color.WHITE);
-        labelStyle.font.getData().setScale(0.4f);
+
         Label coinAmountLabel=new Label(""+GameProgress.currentGold, labelStyle);
         coinAmountLabel.setPosition(coinImage.getX()+coinImage.getWidth()+3, coinImage.getY()+(coinImage.getHeight()-coinAmountLabel.getHeight())/2);
         uiStage.addActor(coinAmountLabel);
@@ -119,5 +173,11 @@ public class CharacterSelectionScreen extends DefaultScreen {
         Gdx.input.setInputProcessor(null);
         uiStage.dispose();
         super.dispose();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+        uiStage.getViewport().update(width, height, true);
     }
 }
